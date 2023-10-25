13695de5632d3ae599c3724edc812b99911f340b
Elton John Zafra <136724817+Eljohnz@users.noreply.github.com>
2023-10-25 22:10:11 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector(".menu-icon").addEventListener("click", function () {
+    document.querySelector(".nav__list").classList.toggle("active");
+    document.querySelector(".menu-icon").classList.toggle("active");
+});
